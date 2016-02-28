ec28bd09bf127ee4f1f652b5ce0b971950f9e7b8
Lamar1 <gouldl2@uni.coventry.ac.uk>
2016-02-28 14:26:35 +0000

    Update main_interface.py

--- gui/main_interface.py
+++ gui/main_interface.py
@@ -57,6 +57,21 @@ class MainInterface:
             self.image = tk.PhotoImage(file="arrows.gif")
             self.expandButton = Button(self.frameBasket, image=self.image, background="white", command = lambda: expandedBasket())
             self.expandButton.place(anchor="c", relx=0.9, rely=0.06)
+            
+            #Creates the related timer buttons & adds them to 'frameTimer',_Button1-3 is the duration selected in seconds
+            self._button1 = Button(self.frameTimer, text='1 minute', command= lambda a=60: clockInput(a))
+            self._button2 = Button(self.frameTimer, text='2 minutes', command= lambda b=120: clockInput(b))
+            self._button3 = Button(self.frameTimer, text='4 minutes', command= lambda c=240: clockInput(c))
+            self._button4 = Button(self.frameTimer, text='Start', command = lambda: count_down(_duration))
+            
+            self._button1.pack(side=LEFT,fill=BOTH,expand=True)
+            self._button2.pack(side=LEFT,fill=BOTH,expand=True)
+            self._button3.pack(side=LEFT,fill=BOTH,expand=True)
+            self._button4.pack(side=LEFT,fill=BOTH,expand=True)
+          
+            def clockInput(Length):
+             '''Saves the duration selected by a timer button'''
+             _duration = Length
 
 
         def labels(self):
@@ -76,29 +91,14 @@ class MainInterface:
             if duration is None:
                 _duration = 60
             for t in range(_duration, -1, -1):
-        # formats duration as 2 digit integers 00:00
-        # divmod() converts the seconds to minutes, seconds
-            sf = "{:02d}:{:02d}".format(*divmod(t, 60))
-            time_str.set(sf)
-            root.update()
-        #delays number changing for 1 second 
-            time.sleep(1)
+                # formats duration as 2 digit integers 00:00
+                # divmod() converts the seconds to minutes, seconds
+                sf = "{:02d}:{:02d}".format(*divmod(t, 60))
+                time_str.set(sf)
+                root.update()
+                #delays number changing for 1 second
+                time.sleep(1)
         
-        #Creates the related timer buttons & adds them to 'frameTimer',_Button1-3 is the duration selected in seconds
-        _button1 = Button(frameTimer, text='1 minute', command= lambda a=60: clockInput(a))
-        _button2 = Button(frameTimer, text='2 minutes', command= lambda b=120: clockInput(b))
-        _button3 = Button(frameTimer, text='4 minutes', command= lambda c=240: clockInput(c))
-        _button4 = Button(frameTimer, text='Start', command = lambda: count_down(_duration))
-            
-        _button1.pack(side=LEFT,fill=BOTH,expand=True)
-        _button2.pack(side=LEFT,fill=BOTH,expand=True)
-        _button3.pack(side=LEFT,fill=BOTH,expand=True)
-        _button4.pack(side=LEFT,fill=BOTH,expand=True)
-       
-        
-        def clockInput(Length):
-            '''Saves the duration selected by a timer button'''
-                _duration = Length
                 
         def distributeItems():
             '''TODO: Randomly places items within map & start timer'''
