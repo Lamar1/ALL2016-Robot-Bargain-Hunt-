4d6630c35ec38b68b5f5e768d9ddc2e6dbc9cc23
Nick Coupland <Nick-Coupland@users.noreply.github.com>
2016-02-29 14:43:34 +0000

    Update Insertion.Sort.py

--- sortalgorithms/Insertion.Sort.py
+++ sortalgorithms/Insertion.Sort.py
@@ -5,7 +5,7 @@ def insertionSort(wordList):
                "J":10, "K":11, "L":12, "M":13, "N":14, "O":15, "P":16, "Q":17,
                "R":18, "S":19, "T":20, "U":21, "V":22, "W":23, "X":24, "Y":25,
                "Z":26}
-    for i in range(1, len(wordList) - 1):
+    for i in range(len(wordList)):
         comparisonValue = wordList[i]
         position = i
         while (position > 0) and (wordList[position-1] > comparisonValue):
