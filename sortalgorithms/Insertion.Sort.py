e2eb26788b6f84fa7f63aac8b402b35c10708aeb
Lamar1 <gouldl2@uni.coventry.ac.uk>
2016-03-02 12:46:04 +0000

    Update Insertion.Sort.py

--- sortalgorithms/Insertion.Sort.py
+++ sortalgorithms/Insertion.Sort.py
@@ -1,21 +1,14 @@
 #Algorithm for sorting a list of words into alphabetical order using insertion sort.
 
-def insertionSort(wordList):
+def insertionSort():
     letterOrder = {"A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, "H":8, "I":9,
                "J":10, "K":11, "L":12, "M":13, "N":14, "O":15, "P":16, "Q":17,
                "R":18, "S":19, "T":20, "U":21, "V":22, "W":23, "X":24, "Y":25,
                "Z":26}
-    for i in range(len(wordList)):
-        comparisonValue = wordList[i]
+    for i in range(len(_List4Table)):
+        comparisonValue = _List4Table[i]
         position = i
-        while (position > 0) and (wordList[position-1] > comparisonValue):
-            wordList[position] = wordList[position-1]
+        while (position > 0) and (_List4Table[position-1] > comparisonValue):
+            _List4Table[position] = _List4Table[position-1]
             position -= 1
-        wordList[position] = comparisonValue
-
-import random
-import string
-list1 = [random.choice(string.ascii_uppercase) for i in range(10)]
-print ("Before: ", list1)
-insertionSort(list1)
-print ("After: ", list1)
+        _List4Table[position] = comparisonValue
