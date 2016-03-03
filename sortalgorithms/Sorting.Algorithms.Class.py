3326a22e041dafe39e005d0ff2b6281a2b7816d4
Lamar1 <gouldl2@uni.coventry.ac.uk>
2016-03-03 14:28:40 +0000

    Update Sorting.Algorithms.Class.py

--- sortalgorithms/Sorting.Algorithms.Class.py
+++ sortalgorithms/Sorting.Algorithms.Class.py
@@ -1,78 +1,78 @@
 class SortAlgorithms:
 
-    def bubbleSort(wordList):
+    def bubbleSort():
     #Function to sort a list into alphabetical/numerical order using Bubble Sort algorithm
-        for i in range(len(wordList) - 1):
-            for j in range(len(wordList) - 1):
-                if (wordList[j])[0] > (wordList[j+1])[0]:
-                    wordList[j], wordList[j+1] = wordList[j+1], wordList[j]
-        return wordList
+        for i in range(len(_List4Table) - 1):
+            for j in range(len(_List4Table) - 1):
+                if (_List4Table[j])[0] > (_List4Table[j+1])[0]:
+                    _List4Table[j], _List4Table[j+1] = _List4Table[j+1], _List4Table[j]
+        return _List4Table
 
-    def bubbleSortReverse(wordList):
+    def bubbleSortReverse():
     #Function to sort a list into reverse alphabetical/numerical order using Bubble Sort algorithm
-        for i in range(len(wordList) - 1):
-            for j in range(len(wordList) - 1):
-                if (wordList[j])[0] < (wordList[j+1])[0]:
-                    wordList[j], wordList[j+1] = wordList[j+1], wordList[j]
-        return wordList
+        for i in range(len(_List4Table) - 1):
+            for j in range(len(_List4Table) - 1):
+                if (_List4Table[j])[0] < (_List4Table[j+1])[0]:
+                    _List4Table[j], _List4Table[j+1] = _List4Table[j+1], _List4Table[j]
+        return _List4Table
 
 
-    def insertionSort(wordList):
+    def insertionSort():
     #Function to sort a list into alphabetical/numerical order using Insertion Sort algorithm
-        for i in range(len(wordList)):
-            comparisonValue = wordList[i]
-            while (i > 0) and (wordList[i-1] > comparisonValue):
-                wordList[i] = wordList[i-1]
+        for i in range(len(_List4Table)):
+            comparisonValue = _List4Table[i]
+            while (i > 0) and (_List4Table[i-1] > comparisonValue):
+                _List4Table[i] = _List4Table[i-1]
                 i -= 1
-            wordList[i] = comparisonValue
-        return wordList
+            _List4Table[i] = comparisonValue
+        return _List4Table
 
-    def insertionSortReverse(wordList):
+    def insertionSortReverse():
     #Function to sort a list into reverse alphabetical/numerical order using Insertion Sort algorithm
-        for i in range(len(wordList)):
-            comparisonValue = wordList[i]
-            while (i > 0) and (wordList[i-1] < comparisonValue):
-                wordList[i] = wordList[i-1]
+        for i in range(len(_List4Table)):
+            comparisonValue = _List4Table[i]
+            while (i > 0) and (_List4Table[i-1] < comparisonValue):
+                _List4Table[i] = _List4Table[i-1]
                 i -= 1
-            wordList[i] = comparisonValue
-        return wordList
+            _List4Table[i] = comparisonValue
+        return _List4Table
 
 
-    def quickSort(wordList):
+    def quickSort():
     #Function to sort a list into alphabetical/numerical order using Quick Sort algorithm
         lessThan = []
         equalTo = []
         greaterThan = []
-        if len(wordList) > 1:
-            pivot = wordList[0]
-            for i in range(len(wordList)):
-                if wordList[i] < pivot:
-                    lessThan.append(wordList[i])
-                elif wordList[i] > pivot:
-                    greaterThan.append(wordList[i])
+        if len(_List4Table) > 1:
+            pivot = _List4Table[0]
+            for i in range(len(_List4Table)):
+                if _List4Table[i] < pivot:
+                    lessThan.append(_List4Table[i])
+                elif _List4Table[i] > pivot:
+                    greaterThan.append(_List4Table[i])
                 else:
-                    equalTo.append(wordList[i])
+                    equalTo.append(_List4Table[i])
             return quickSort(lessThan) + equalTo + quickSort(greaterThan)
         else:
-            return wordList
+            return _List4Table
 
-    def quickSortReverse(wordList):
+    def quickSortReverse():
     #Function to sort a list into  reverse alphabetical/numerical order using Quick Sort algorithm
         lessThan = []
         equalTo = []
         greaterThan = []
-        if len(wordList) > 1:
-            pivot = wordList[0]
-            for i in range(len(wordList)):
-                if wordList[i] < pivot:
-                    lessThan.append(wordList[i])
-                elif wordList[i] > pivot:
-                    greaterThan.append(wordList[i])
+        if len(_List4Table) > 1:
+            pivot = _List4Table[0]
+            for i in range(len(_List4Table)):
+                if _List4Table[i] < pivot:
+                    lessThan.append(_List4Table[i])
+                elif _List4Table[i] > pivot:
+                    greaterThan.append(_List4Table[i])
                 else:
-                    equalTo.append(wordList[i])
+                    equalTo.append(_List4Table[i])
             return quickSortReverse(greaterThan) + equalTo + quickSortReverse(lessThan)
         else:
-            return wordList
+            return _List4Table
 
 import random
 import string
